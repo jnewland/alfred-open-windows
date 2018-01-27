a975d3c048364c353d3acbda8f8bcc49ba54a373
Jesse Newland <jesse@jnewland.com>
2018-01-27 11:01:40 -0600

    use alfred's built-in filtering

--- alfred-open-windows-list.sh
+++ alfred-open-windows-list.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 exec 2> >(logger -s -t $(basename $0))
 
-FILTER="$1"
-if [ -z "$FILTER" ]; then FILTER=.;fi
-
 APPLICATION=$(osascript -sh application.ascr "$focusedapp" | sed "s/Macintosh HD//" | sed "s/:/\//g" | sed "s/\/$//")
 
 echo '<?xml version="1.0"?><items>'
@@ -11,15 +8,6 @@ echo '<?xml version="1.0"?><items>'
 osascript -sh windows.ascr "$focusedapp" | tr "\r" "\n" | while read LINE; do
     if [ "$LINE" = "" ]; then
         continue
-    fi
-    if [ "$FILTER" = "" ]; then
-        continue
-    else
-        if echo $LINE | grep -sqi $FILTER; then
-          true
-        else
-          continue
-        fi
     fi
     echo "<item arg=\"${LINE}\">"
     echo "  <title>${LINE}</title>"
